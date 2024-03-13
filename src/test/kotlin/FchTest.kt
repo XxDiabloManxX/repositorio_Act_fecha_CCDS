2bc13dcd9d25bb2ea4a32a3349fa8981e07e6f58
Carlos <carlos.canal.da.silva@gmail.com>
2024-03-13 19:43:50 +0100

    Pruebas unitarias

--- src/test/kotlin/FchTest.kt
+++ src/test/kotlin/FchTest.kt
@@ -5,11 +5,15 @@ import org.junit.jupiter.api.Assertions.*
 
 class FchTest {
 
+    var fecha : Fch = Fch(0, 0, 0)
+
     @BeforeEach
     fun setUp() {
+        fecha = Fch(21, 6, 2012)
     }
 
     @Test
     fun valida() {
+        assertTrue(fecha.valida())
     }
 }
