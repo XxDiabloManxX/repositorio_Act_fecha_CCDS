4127913b2013d3499341660c7fe37e6801320d9e
Carlos <carlos.canal.da.silva@gmail.com>
2024-03-13 19:42:17 +0100

    Pruebas unitarias

---
+++ src/test/kotlin/FchTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class FchTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun valida() {
+    }
+}
